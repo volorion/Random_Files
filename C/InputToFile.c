24bcfd7f5b96940191b5943ec7b69007212a44e5
gmorris <gerardmorris181187@gmail.com>
2020-09-27 13:28:43 +0100

    Base

---
+++ C/InputToFile.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY_INPUT     -1
+#define TOO_LONG        -2
+
+int ReadInput (char *buff, size_t maxSize)
+{
+	int ch, extra;
+    if (fgets (buff, maxSize, stdin) == NULL)
+	{
+        return EMPTY_INPUT;
+	}
+
+	long buffSize = strlen(buff) - 1;
+	if(buffSize > maxSize)
+    {
+        return TOO_LONG;
+    }
+
+    buff[strlen(buff)-1] = '\0';
+    return buffSize;
+}
+
+int WriteToFile (char *buff, int len)
+{
+    FILE *outFile = fopen ("out.txt", "w");
+    printf("Writing contents into output file.\n");
+    fwrite (buff, len, 1, outFile);
+}
+
+int main (void) {
+    int rc;
+    char inputBuffer[1024];
+
+	printf("Please input characters (max len of 1024).\n");
+
+    int response = ReadInput ( inputBuffer, 1024);
+    if (response == EMPTY_INPUT) {
+        printf ("\nNo input\n");
+        return EMPTY_INPUT;
+    }
+    else if (rc == TOO_LONG) {
+        printf ("Input too long (max 1024)\n");
+        return TOO_LONG;
+    }
+
+    WriteToFile(inputBuffer, response);
+    printf ("Writing [%s]\n", inputBuffer);
+    return 0;
+}
